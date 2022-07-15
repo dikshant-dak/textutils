48e97be4d8df124c8d958d417adbb3c6aba920be
Dikshant Dak <dikshant.dak@gmail.com>
2022-07-15 22:15:14 +0530

    Updates

---
+++ precache-manifest.088d277bf8f933612d4467948b5788a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dbbc76494c62b45569b",
+    "url": "/textutils/static/css/main.db3258cb.chunk.css"
+  },
+  {
+    "revision": "4dbbc76494c62b45569b",
+    "url": "/textutils/static/js/main.4dbbc764.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textutils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textutils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8c5d3a7ccc996c2dc493",
+    "url": "/textutils/static/js/runtime~main.8c5d3a7c.js"
+  },
+  {
+    "revision": "f925653d54d87c5bec98ae3fe1e37c36",
+    "url": "/textutils/index.html"
+  }
+];
